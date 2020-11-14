4ea510e870852d31f9b28293789dac669f07bb05
quavious <leenowon97@gmail.com>
2020-11-14 12:36:52 +0900

    Use GetStaticProps

---
+++ pages/dynamic/dynamic.tsx
@@ -0,0 +1,25 @@
+import {GetStaticProps} from 'next'
+import path from 'path'
+
+// Executed on server
+export const getStaticProps: GetStaticProps = async context => {
+
+    const fs = require('fs')
+    
+    return {
+        props: {
+            myFavoriteNumber : Math.floor(Math.random() * 100),
+            myText : fs.readFileSync(path.join(process.cwd(), 'public/robots.txt'), 'utf-8')
+        }
+    }
+}
+
+export default function DynamicPage(props){
+    return (
+        <>
+            <h1>It is dynamic page.</h1>
+            <p>{props.myFavoriteNumber}</p>
+            <p>{props.myText}</p>
+        </>
+    )
+}
